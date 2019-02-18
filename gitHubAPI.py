a0f3ea3cfc42b1891e95905e2aa0ca1a720b9908
Alex Supkay <asupkay1124@gmail.com>
2019-02-17 20:08:17 -0500

    Initial implementation

--- gitHubAPI.py
+++ gitHubAPI.py
@@ -1,4 +1,28 @@
-def commitCount(gitHubID):
-    print("jere")
+import requests
+import json
 
-commitCount(123);
+def commitCount(gitHubID):
+    repos = getRepositories(gitHubID)
+    print(repos)
+    dict = {};
+    for repo in repos:
+        repoName = repo["name"]
+        dict[repoName] = getCommits(gitHubID, repoName)
+
+    print(dict)
+        
+
+def getRepositories(gitHubID):
+    URL = "https://api.github.com/users/" + gitHubID + "/repos"
+    r = requests.get(url = URL);
+    data = r.json()
+    return data
+
+def getCommits(gitHubID, repoName):
+    URL = "https://api.github.com/repos/" + gitHubID + "/" + repoName + "/commits"
+    r = requests.get(url = URL);
+    data = r.json()
+    print(data)
+    return len(data)
+
+commitCount("Bsoong");
